6b049053e2305aa7fde2a11f637e18ff60a56b6e
Fortaleza01 <fortalezacucuta1@gmail.com>
2025-07-17 13:30:04 +0000

    Actualización de archivos GeoJSON optimizados para el visor

--- script.js
+++ script.js
@@ -1,134 +1,63 @@
-const map = L.map("map").setView([7.8998, -72.5487], 16);
+// Crear el mapa centrado en La Fortaleza, Cúcuta
+var map = L.map('map').setView([7.903, -72.55], 16);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+// Capa base de OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 20,
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-const capas = {};
-const controlCapas = L.control.layers(null, null, { collapsed: false }).addTo(map);
+// Estilos por capa
+function estiloPerimetro() {
+  return { color: '#000', weight: 2, fill: false };
+}
+function estiloSectores() {
+  return { color: '#006400', weight: 1, fillColor: '#90ee90', fillOpacity: 0.3 };
+}
+function estiloVias() {
+  return { color: '#ff0000', weight: 2 };
+}
+function estiloConstrucciones() {
+  return { color: '#0000ff', weight: 0.5, fillColor: '#0000ff', fillOpacity: 0.6 };
+}
+function estiloEncuestas() {
+  return { radius: 4, fillColor: '#ffa500', color: '#fff', weight: 1, opacity: 1, fillOpacity: 0.9 };
+}
 
-let construccionesLayer, encuestasLayer, sectoresLayer, perimetroLayer, viasLayer;
+// Cargar y añadir capas GeoJSON
+Promise.all([
+  fetch('data/PERIMETRO.json').then(res => res.json()),
+  fetch('data/SECTORES.json').then(res => res.json()),
+  fetch('data/VIAS.json').then(res => res.json()),
+  fetch('data/CONSTRUCCIONES.json').then(res => res.json()),
+  fetch('data/ENCUESTAS.json').then(res => res.json())
+]).then(([perimetro, sectores, vias, construcciones, encuestas]) => {
+  const capaPerimetro = L.geoJSON(perimetro, { style: estiloPerimetro });
+  const capaSectores = L.geoJSON(sectores, { style: estiloSectores });
+  const capaVias = L.geoJSON(vias, { style: estiloVias });
+  const capaConstrucciones = L.geoJSON(construcciones, { style: estiloConstrucciones });
+  const capaEncuestas = L.geoJSON(encuestas, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, estiloEncuestas());
+    }
+  });
 
-const archivos = [
-  { nombre: "Perímetro", archivo: "data/PERIMETRO_FORTALEZA.json", color: "#000" },
-  { nombre: "Sectores", archivo: "data/SECTORES_FORTALEZA.json", color: "#009688" },
-  { nombre: "Vías", archivo: "data/VIAS_FORTALEZA.json", color: "#f00" },
-  { nombre: "Construcciones", archivo: "data/CONSTRUCCIONES_FORTALEZA.json", color: "#999" },
-  { nombre: "Encuestas", archivo: "data/ENCUESTAS_FORTALEZA.json", color: "#007bff" }
-];
+  // Añadir capas al mapa
+  capaPerimetro.addTo(map);
+  capaSectores.addTo(map);
+  capaVias.addTo(map);
+  capaConstrucciones.addTo(map);
+  capaEncuestas.addTo(map);
 
-archivos.forEach(capa => {
-  fetch(capa.archivo)
-    .then(res => res.json())
-    .then(data => {
-      const layer = L.geoJSON(data, {
-        style: { color: capa.color, weight: 1, fillOpacity: 0.5 },
-        pointToLayer: (feature, latlng) => L.circleMarker(latlng, {
-          radius: 4,
-          fillColor: capa.color,
-          color: "#000",
-          weight: 0.5,
-          opacity: 1,
-          fillOpacity: 0.8
-        })
-      }).addTo(map);
+  // Control de capas
+  var capasBase = {};
+  var capasOverlay = {
+    'Perímetro': capaPerimetro,
+    'Sectores': capaSectores,
+    'Vías': capaVias,
+    'Construcciones': capaConstrucciones,
+    'Encuestas': capaEncuestas
+  };
 
-      capas[capa.nombre] = layer;
-      controlCapas.addOverlay(layer, capa.nombre);
-
-      if (capa.nombre === "Construcciones") construccionesLayer = layer;
-      if (capa.nombre === "Encuestas") encuestasLayer = layer;
-      if (capa.nombre === "Sectores") sectoresLayer = layer;
-      if (capa.nombre === "Perímetro") {
-        perimetroLayer = layer;
-        map.fitBounds(layer.getBounds());  // Ajuste automático al perímetro
-      }
-      if (capa.nombre === "Vías") viasLayer = layer;
-    });
-});
-
-// Leyenda
-const legend = L.control({ position: "bottomright" });
-legend.onAdd = function () {
-  const div = L.DomUtil.create("div", "info legend");
-  div.innerHTML += "<h4>Leyenda</h4>";
-  div.innerHTML += '<i style="background: #000"></i> Perímetro<br>';
-  div.innerHTML += '<i style="background: #009688"></i> Sectores<br>';
-  div.innerHTML += '<i style="background: #f00"></i> Vías<br>';
-  div.innerHTML += '<i style="background: #999"></i> Construcciones<br>';
-  div.innerHTML += '<i style="background: #007bff"></i> Encuestas<br>';
-  return div;
-};
-legend.addTo(map);
-
-// Filtro
-document.getElementById("sectorSelect").addEventListener("change", e => {
-  const sector = e.target.value;
-
-  // Construcciones
-  construccionesLayer.clearLayers();
-  fetch("data/CONSTRUCCIONES_FORTALEZA.json")
-    .then(res => res.json())
-    .then(data => {
-      const filtrado = {
-        ...data,
-        features: sector === "TODOS" ? data.features :
-          data.features.filter(f => f.properties.SECTOR === sector)
-      };
-      construccionesLayer.addData(filtrado);
-    });
-
-  // Encuestas
-  encuestasLayer.clearLayers();
-  fetch("data/ENCUESTAS_FORTALEZA.json")
-    .then(res => res.json())
-    .then(data => {
-      const filtrado = {
-        ...data,
-        features: sector === "TODOS" ? data.features :
-          data.features.filter(f => f.properties.SECTOR === sector)
-      };
-      encuestasLayer.addData(filtrado);
-    });
-
-  // Sectores + centrar
-  sectoresLayer.clearLayers();
-  fetch("data/SECTORES_FORTALEZA.json")
-    .then(res => res.json())
-    .then(data => {
-      const filtrado = {
-        ...data,
-        features: sector === "TODOS" ? data.features :
-          data.features.filter(f => f.properties.SECTOR === sector)
-      };
-      sectoresLayer.addData(filtrado);
-
-      if (sector !== "TODOS" && filtrado.features.length > 0) {
-        const bounds = L.geoJSON(filtrado).getBounds();
-        map.fitBounds(bounds);
-      } else {
-        map.setView([7.8998, -72.5487], 16);  // Volver al centro original
-      }
-    });
-
-  // Perímetro
-  perimetroLayer.clearLayers();
-  fetch("data/PERIMETRO_FORTALEZA.json")
-    .then(res => res.json())
-    .then(data => {
-      perimetroLayer.addData(data);
-    });
-
-  // Vías
-  viasLayer.clearLayers();
-  fetch("data/VIAS_FORTALEZA.json")
-    .then(res => res.json())
-    .then(data => {
-      const filtrado = {
-        ...data,
-        features: sector === "TODOS" ? data.features :
-          data.features.filter(f => f.properties.SECTOR === sector)
-      };
-      viasLayer.addData(filtrado);
-    });
+  L.control.layers(capasBase, capasOverlay).addTo(map);
 });
